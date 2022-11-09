2fd6e3c7fdec62c084952d0755f889f40950a15e
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-09 17:13:42 +0800

    计数器

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  state={
+    count:0
+  }
   render() {
+    let {count}=this.state;
     return (
       <div>
         <h3>App组件</h3>
+        <p>count:{count}</p>
+        <button onClick={()=>{
+          this.setState({count:count+1})
+        }}>+</button>
       </div>
     )
   }
